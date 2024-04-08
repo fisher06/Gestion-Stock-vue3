2e63713699668cf163a142db8f64606d5f93f1d2
Wei XIE <wei.xie8906@gmail.com>
2024-04-08 12:12:23 +0200

    petit serveur web

--- back/server.js
+++ back/server.js
@@ -1,2 +1,14 @@
 // first commit
-console.log("test server.js, start the server");
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
