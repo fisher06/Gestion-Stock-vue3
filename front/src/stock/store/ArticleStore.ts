96ba0e8abf06870d83f308d9dd1c1dc41b8adb1a
Wei XIE <wei.xie8906@gmail.com>
2024-04-09 16:54:38 +0200

    store

--- front/src/stock/store/ArticleStore.ts
+++ front/src/stock/store/ArticleStore.ts
@@ -1,22 +1,40 @@
 import { defineStore } from 'pinia'
 import type { Article, NewArticle } from '../interfaces/Article'
 import { api } from '../api'
+import { ref } from 'vue'
 
-export const useArticleStore = defineStore('article', {
-  state: () => ({
-    articles: undefined as Article[] | undefined
-  }),
-  actions: {
-    async add(newArticle: NewArticle) {
-      await api.add(newArticle)
-      this.articles = await api.getArticles()
-    },
-    async remove(ids: Article['id'][]) {
-      await api.remove(ids)
-      this.articles = await api.getArticles()
-    },
-    async refresh() {
-      this.articles = await api.getArticles()
-    }
+// export const useArticleStore = defineStore('article', {
+//   state: () => ({
+//     articles: undefined as Article[] | undefined
+//   }),
+//   actions: {
+//     async add(newArticle: NewArticle) {
+//       await api.add(newArticle)
+//       this.articles = await api.getArticles()
+//     },
+//     async remove(ids: Article['id'][]) {
+//       await api.remove(ids)
+//       this.articles = await api.getArticles()
+//     },
+//     async refresh() {
+//       this.articles = await api.getArticles()
+//     }
+//   }
+// })
+
+export const useArticleStore = defineStore('article', () => {
+  const articles = ref<Article[] | undefined>()
+  const add = async (newArticle: NewArticle): Promise<void> => {
+    await api.add(newArticle)
+    articles.value = await api.getArticles()
   }
+
+  const remove = async (ids: Article['id'][]): Promise<void> => {
+    await api.remove(ids)
+    articles.value = await api.getArticles()
+  }
+  const refresh = async (): Promise<void> => {
+    articles.value = await api.getArticles()
+  }
+  return { articles, add, remove, refresh }
 })
