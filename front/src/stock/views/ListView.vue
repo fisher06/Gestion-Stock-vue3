2fb659720284fafa32e4bcd96952bbcc87482ff6
Wei XIE <wei.xie8906@gmail.com>
2024-04-09 17:28:35 +0200

    list article

--- front/src/stock/views/ListView.vue
+++ front/src/stock/views/ListView.vue
@@ -1,65 +1,54 @@
-<script lang="ts">
+<script setup lang="ts">
 import type { Article } from '../interfaces/Article'
 import { useArticleStore } from '../store/ArticleStore'
 import AsyncButton from '@/components/AsyncButton.vue'
 import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+import { onMounted } from 'vue'
 
-export default {
-  name: 'ListView',
-  components: {
-    AsyncButton,
-    RouterLink
-  },
-  data() {
-    return {
-      selectedArticles: new Set<Article>(),
-      errorMsg: ''
-    }
-  },
-  computed: {
-    articles() {
-      const articleStore = useArticleStore()
-      return articleStore.articles
-    }
-  },
-  methods: {
-    select(a: Article) {
-      console.log('select')
-      this.selectedArticles.has(a) ? this.selectedArticles.delete(a) : this.selectedArticles.add(a)
-      this.selectedArticles = new Set(this.selectedArticles)
-    },
-    async remove() {
-      const articleStore = useArticleStore()
-      try {
-        this.errorMsg = ''
-        const ids = [...this.selectedArticles].map((a: Article) => a.id)
-        await articleStore.remove(ids)
-        this.selectedArticles.clear()
-      } catch (err) {
-        console.log('err: ', err)
-        this.errorMsg = 'Erreur Technique'
-      }
-    },
-    async refresh() {
-      const articleStore = useArticleStore()
-      try {
-        this.errorMsg = ''
-        console.log('refreshing')
-        await articleStore.refresh()
-        console.log('refreshed')
-      } catch (err) {
-        console.log('err: ', err)
-        this.errorMsg = 'Erreur Technique'
-      }
-    }
-  },
-  mounted() {
-    const articleStore = useArticleStore()
-    if (articleStore.articles === undefined) {
-      articleStore.refresh()
-    }
+const selectedArticles = ref(new Set<Article>())
+const errorMsg = ref('')
+const articleStore = useArticleStore()
+
+const addViewRoute = {
+  name: 'stockAdd'
+}
+
+const select = (a: Article) => {
+  console.log('select')
+  selectedArticles.value.has(a) ? selectedArticles.value.delete(a) : selectedArticles.value.add(a)
+  selectedArticles.value = new Set(selectedArticles.value)
+}
+
+const remove = async () => {
+  try {
+    errorMsg.value = ''
+    const ids = [...selectedArticles.value].map((a: Article) => a.id)
+    await articleStore.remove(ids)
+    selectedArticles.value.clear()
+  } catch (err) {
+    console.log('err: ', err)
+    errorMsg.value = 'Erreur Technique'
   }
 }
+
+const refresh = async () => {
+  try {
+    errorMsg.value = ''
+    console.log('refreshing')
+    await articleStore.refresh()
+    console.log('refreshed')
+  } catch (err) {
+    console.log('err: ', err)
+    errorMsg.value = 'Erreur Technique'
+  }
+}
+
+onMounted(() => {
+  if (articleStore.articles === undefined) {
+    articleStore.refresh()
+  }
+})
 </script>
 
 <template>
@@ -69,7 +58,7 @@ export default {
       <nav>
         <AsyncButton title="Rafraîchir" :action="refresh" icon="fa-solid fa-rotate-right">
         </AsyncButton>
-        <router-link append :to="{ name: 'stockAdd' }" class="button" title="Ajouter">
+        <router-link append :to="addViewRoute" class="button" title="Ajouter">
           <fa-icon icon="fa-solid fa-plus" />
         </router-link>
         <AsyncButton
@@ -91,7 +80,7 @@ export default {
         </thead>
         <tbody>
           <tr
-            v-for="a in articles"
+            v-for="a in articleStore.articles"
             :key="a.id"
             @click="select(a)"
             :class="{ selected: selectedArticles.has(a) }"
@@ -100,7 +89,7 @@ export default {
             <td class="price number">{{ a.price }} €</td>
             <td class="qty number">{{ a.qty }}</td>
           </tr>
-          <tr v-if="articles === undefined">
+          <tr v-if="articleStore.articles === undefined">
             <td colspan="3">
               <div class="loading">
                 <fa-icon icon="fa-solid fa-circle-notch" :spin="true"></fa-icon>
